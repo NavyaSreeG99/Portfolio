f2f95088850c1d41ce5212d2618a2ada6e1a415c
NavyaSreeG99 <ngavini@albany.edu>
2022-09-30 15:35:38 -0400

    First Commit

---
+++ test_express.js
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+var express = require('express');  
+var app = express();  
+app.get('/', function (req, res) {  
+   res.send('Welcome to JavaTpoint');  
+})  
+var server = app.listen(8000, function () {  
+var host = server.address().address  
+  var port = server.address().port  
+ console.log("Example app listening at http://%s:%s", host, port)  
+})  
